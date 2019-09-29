39c3d716c9cec99361d7f21aadb65231febbf8b4
Mirino97 <j.cesarueti@yahoo.com.br>
2019-09-29 19:05:40 -0300

    Fuck

---
+++ resources/views/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="<?php echo asset('css/theme.css')?>" type="text/css">
+</head>
+<body>
+
+
+    <div id="navbar">
+        <div id="navbar-logo">
+            <p>LOGO</p>
+        </div>
+        <div id="filler"></div>
+        <button class="button-home">
+            Home
+        </button>
+        <button class="button-home">
+            HTML
+        </button>
+        <button class="button-home">
+            CSS
+        </button>
+        <button class="button-home">
+            Laravel
+        </button>
+    </div>
+
+</body>
+</html>
